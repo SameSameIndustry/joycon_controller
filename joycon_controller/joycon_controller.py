08da9f8a412e81a9d1274b5a90de0cd7c0fc94cf
ry0py <ryoppy101@gmail.com>
2025-10-25 19:57:34 +0900

    lも追加

--- joycon_controller/joycon_controller.py
+++ joycon_controller/joycon_controller.py
@@ -7,21 +7,30 @@ from joycon_controller.joycon_ahrs_rumble import JoyConAHRSRumbler
 class JoyConController(Node):
     def __init__(self):
         super().__init__('joycon_controller')
-        self.jc = JoyConAHRSRumbler("R", alpha=0.03)
-        self.pub_pitch = self.create_publisher(Float64, '~/ref_pitch', 10)
+        self.jc_r = JoyConAHRSRumbler("R", alpha=0.03)
+        self.jc_l = JoyConAHRSRumbler("L", alpha=0.03)
+        self.pub_pitch_r = self.create_publisher(Float64, '~/ref_pitch_r', 10)
+        self.pub_pitch_l = self.create_publisher(Float64, '~/ref_pitch_l', 10)
         self.sub_rumble = self.create_subscription(Float64, '~/rumble', self.on_rumble, 10)
         self.create_timer(0.01, self.on_timer)  # 100 Hz
-        self.jc.recenter_yaw()
+        self.jc_r.recenter_yaw()
+        self.jc_l.recenter_yaw()
 
     def on_timer(self):
-        pitch, roll, yaw = self.jc.get_euler_rad()  # (pitch, roll, yaw) に整形済みとするなら適宜変更
+        pitch, roll, yaw = self.jc_r.get_euler_rad()  # (pitch, roll, yaw) に整形済みとするなら適宜変更
         msg = Float64()
         msg.data = yaw
-        self.pub_pitch.publish(msg)
+        self.pub_pitch_r.publish(msg)
+
+        pitch, roll, yaw = self.jc_l.get_euler_rad()
+        msg = Float64()
+        msg.data = yaw
+        self.pub_pitch_l.publish(msg)
 
     def on_rumble(self, msg: Float64):
         amp = float(msg.data)
-        self.jc.rumble(amplitude=abs(amp), side="R" if amp >= 0 else "L", duration_ms=120)
+        self.jc_r.rumble(amplitude=abs(amp), side="R" if amp >= 0 else "L", duration_ms=120)
+        self.jc_l.rumble(amplitude=abs(amp), side="R" if amp >= 0 else "L", duration_ms=120)
 
 def main():
     rclpy.init()
